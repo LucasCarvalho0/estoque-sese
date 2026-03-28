677b4dd5de47e6910b6e83f2e30c09db233afd4f
Your Name <you@example.com>
2026-03-28 14:30:01 -0300

    chore: wipe index

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-import { ShiftConfig } from './types';
-
-export const SHIFTS: ShiftConfig[] = [
-  { id: '1', label: 'Primeiro Turno', email: 'turno1@sese.com', password: 'Kiezen.turno1' },
-  { id: '2', label: 'Segundo Turno', email: 'turno2@sese.com', password: 'Kaizenturno.2' },
-];
-
-export const STORAGE_KEY = 'estoque_sese_v1';
